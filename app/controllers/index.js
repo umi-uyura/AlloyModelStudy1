efd7887f82524c7a10ca123edabcee5312c0df4e
Umi Uyura <umiuyura@gmail.com>
2016-02-16 12:34:18 +0900

    コンテンツの編集処理を追加

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -28,30 +28,53 @@ function clickItem(e) {
   var itemId = e.itemId;
   var text = $.memoList.sections[e.sectionIndex].items[e.itemIndex].properties.title;
 
-  dialogs.confirm({
-    title: 'Do you want to delete ?',
-    message: '"' + text + '"',
-    callback: function() {
-      Ti.API.debug('memos = ' + JSON.stringify(memos));
+  if (OS_ANDROID) {
+    itemId = parseInt(itemId);
+  }
 
-      Ti.API.debug('Click id = ' + itemId);
-      Ti.API.debug('Type check = ' + Object.prototype.toString.call(itemId));
+  var opt = {
+    title: 'Action for: ' + text,
+    options: [
+      'Favorite',
+      'Delete',
+      'Cancel'
+    ],
+    cancel: 2,
+    destructive: 1
+  };
 
-      if (OS_ANDROID) {
-        itemId = parseInt(itemId);
-      }
-
+  var optionDialog = Ti.UI.createOptionDialog(opt);
+  optionDialog.addEventListener('click', function(e) {
+    switch (e.index) {
+    case 0:
       var m = memos.where({id: itemId});
-
-      Ti.API.debug('m = ' + JSON.stringify(m));
-
       if (m.length) {
-        m[0].destroy();
-
-        Ti.API.debug('memos = ' + JSON.stringify(memos));
+        var contents = '*' + m[0].get('contents');
+        m[0].set({contents: contents});
+        m[0].save();
       }
+      break;
+    case 1:
+      dialogs.confirm({
+        title: 'Do you want to delete ?',
+        message: '"' + text + '"',
+        callback: function() {
+          var m = memos.where({id: itemId});
+          if (m.length) {
+            m[0].destroy();
+          }
+        }
+      });
+      break;
+    case 2:
+      // Cancel
+      break;
+    default:
+      Ti.API.error('Unknown action = ' + e.index);
+      break;
     }
   });
+  optionDialog.show();
 }
 
 $.index.open();
